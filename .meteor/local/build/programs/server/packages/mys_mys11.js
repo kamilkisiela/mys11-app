6dd9e2f924491c54a03aa2e2d419241590a89892
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-03-29 18:25:35 +0200

    yes

---
+++ .meteor/local/build/programs/server/packages/mys_mys11.js
@@ -0,0 +1,14 @@
+(function () {
+
+/* Imports */
+var Meteor = Package.meteor.Meteor;
+
+
+
+/* Exports */
+if (typeof Package === 'undefined') Package = {};
+Package['mys:mys11'] = {};
+
+})();
+
+//# sourceMappingURL=mys_mys11.js.map
---
+++ .meteor/local/build/programs/web.browser/packages/mys_mys11.js
@@ -0,0 +1,35 @@
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //
+// This is a generated file. You can view the original                  //
+// source in your browser if your browser supports source maps.         //
+// Source maps are supported by all recent versions of Chrome, Safari,  //
+// and Firefox, and by Internet Explorer 11.                            //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+
+
+(function () {
+
+/* Imports */
+var Meteor = Package.meteor.Meteor;
+
+(function(){
+
+///////////////////////////////////////////////////////////////////////
+//                                                                   //
+// packages/mys_mys11/index.js                                       //
+//                                                                   //
+///////////////////////////////////////////////////////////////////////
+                                                                     //
+window // to see ReferenceError                                      // 1
+                                                                     // 2
+///////////////////////////////////////////////////////////////////////
+
+}).call(this);
+
+
+/* Exports */
+if (typeof Package === 'undefined') Package = {};
+Package['mys:mys11'] = {};
+
+})();
